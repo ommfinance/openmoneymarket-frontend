0e21da6d297cef9cb0b2bc15aab705e3c31582fd
RobiMarkac <robi.markac@block42.tech>
2020-11-09 17:43:13 +0100

    server fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3001;
 
 app.use(express.static(__dirname + '/dist/frontend'));
 
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
+app.get('/*', (req, res) => res.sendFile(path.join(config.root, 'index.html')));
 
 const server = http.createServer(app);
 
