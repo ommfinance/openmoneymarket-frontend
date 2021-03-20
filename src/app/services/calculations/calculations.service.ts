5ca82e2c6a8f4c0fddf000ceb4de900296d199cc
RobiMarkac <robi.markac@block42.tech>
2021-03-20 13:10:29 +0100

    full repayment formula conversion fix

--- src/app/services/calculations/calculations.service.ts
+++ src/app/services/calculations/calculations.service.ts
@@ -22,7 +22,9 @@ export class CalculationsService {
   public totalRepaymentFormula(assetTag: AssetTag): number {
     const currentBorrowBalanceUSD = this.persistenceService.getUserBorrowedAssetUSDBalance(assetTag);
     const borrowRate = this.persistenceService.getUserAssetReserve(assetTag)!.borrowRate;
-    const res = currentBorrowBalanceUSD * (1 + borrowRate * (1 / (365 * 24 * 6)));
+    const res = Utils.convertSICXToICX(currentBorrowBalanceUSD * (1 + borrowRate * (1 / (365 * 24 * 6))),
+      this.persistenceService.sIcxToIcxRate());
+
     log.debug("Total repayment formula res = ", res / this.persistenceService.getAssetExchangePrice(assetTag));
     return res / this.persistenceService.getAssetExchangePrice(assetTag);
   }
