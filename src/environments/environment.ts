5b5b2d617a937d68d2cc64bfb81119d66a55ce17
robimarkac <robi.markac@gmail.com>
2021-08-06 17:03:53 +0200

    community testnet address provider, activate rewards timestamps

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   iconRpcUrl: "https://bicon.net.solidwallet.io/api/v3",
   iconDebugRpcUrl: "https://bicon.net.solidwallet.io/api/debug/v3",
-  ADDRESS_PROVIDER_SCORE: 'cxc39e0e3c228dba62db922776936cc784a9727733',
+  ADDRESS_PROVIDER_SCORE: 'cx7645df3f68fb032e925bf01f66c5e77129ef920b',
   BALANCED_DEX_SCORE: "cx399dea56cf199b1c9e43bead0f6a284bdecfbf62",
   ledgerBip32Path: "44'/4801368'/0'/0'",
   GOVERNANCE_ADDRESS: "cx0000000000000000000000000000000000000001",
@@ -14,7 +14,7 @@ export const environment = {
   NID: 3,
   REWARDS_ACCRUE_START: 1628312400, // unix timestamp from when rewards accrue will start
   REWARDS_CLAIMABLE_START: 1628917200, // unix timestamp from when rewards are going to be claimable
-  ACTIVATE_REWARDS_TIMESTAMPS: false, // boolean flag deciding if above two timestamp should be respected
+  ACTIVATE_REWARDS_TIMESTAMPS: true, // boolean flag deciding if above two timestamp should be respected
 };
 
 /*
