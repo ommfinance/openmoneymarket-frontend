6909d152df56bc752a990528677dee6e2967fa5a
robimarkac <robi.markac@gmail.com>
2021-08-23 19:32:31 +0200

    production address provider

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   iconRpcUrl: "https://ctz.solidwallet.io/api/v3",
   iconDebugRpcUrl: "https://ctz.solidwallet.io/api/debug/v3",
-  ADDRESS_PROVIDER_SCORE: 'cx77497caa623b73bb492942081eb809f824b9c82a',
+  ADDRESS_PROVIDER_SCORE: 'cx6a66130200b4f08c65ef394469404378ab52e5b6',
   BALANCED_DEX_SCORE: "cxa0af3165c08318e988cb30993b3048335b94af6c",
   ledgerBip32Path: "44'/4801368'/0'/0'",
   GOVERNANCE_ADDRESS: "cx0000000000000000000000000000000000000001",
