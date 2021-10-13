bcc0b13a443677226fbd7f046fe82f3dd13c9e40
robimarkac <robi.markac@gmail.com>
2021-10-13 20:35:29 +0200

    feature: banner implementation + ICE banner content

---
+++ src/app/components/banner/banner.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {LocalStorageService} from "../../services/local-storage/local-storage.service";
+import {ommBannerExitKey} from "../../common/constants";
+import log from "loglevel";
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.css']
+})
+export class BannerComponent implements OnInit {
+
+  // show banner only if the user has not pressed x on this device (localstorage)
+  showBanner = this.localstorageService.get(ommBannerExitKey) as boolean;
+
+  constructor(private localstorageService: LocalStorageService) { }
+
+  ngOnInit(): void {
+    this.showBanner = this.localstorageService.get(ommBannerExitKey) as boolean;
+    log.debug("showBanner = ", this.showBanner);
+  }
+
+  onBannerExitClick(): void {
+    this.localstorageService.set(ommBannerExitKey, false);
+    this.showBanner = false;
+  }
+}
