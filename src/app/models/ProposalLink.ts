f4f70cb6e37b2757ffaab486a132168c94af1a6a
robimarkac <robi.markac@gmail.com>
2021-12-02 21:53:34 +0100

    feature: forum link

--- src/app/models/ProposalLink.ts
+++
@@ -1,9 +0,0 @@
-export class ProposalLink {
-  forumLink: string;
-  title: string;
-
-  constructor(forumLink: string, title: string) {
-    this.forumLink = forumLink;
-    this.title = title;
-  }
-}
