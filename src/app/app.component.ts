e2ee9210ec5ed1c8b03e7b7dbfb5b492d2efbf57
robimarkac <robi.markac@gmail.com>
2022-07-11 08:18:48 +0200

    add env

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,7 @@ declare var $: any;
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
 
