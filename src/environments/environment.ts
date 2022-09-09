ae259997aa89a8ea6d8a2b575c8f062c2189aebb
robimarkac <robi.markac@gmail.com>
2022-09-09 14:48:42 +0200

    new berlin config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,20 +4,20 @@
 
 export const environment = {
   production: false,
-  iconRpcUrl: "https://sejong.net.solidwallet.io/api/v3",
+  iconRpcUrl: "https://berlin.net.solidwallet.io/api/v3",
   ommRestApi: "https://omm-stats-api-dev-r8jdq.ondigitalocean.app/api/v1",
-  iconDebugRpcUrl: "https://sejong.net.solidwallet.io/api/v3d",
-  ADDRESS_PROVIDER_SCORE: 'cx1e6fcc68f8007b88fdde5503229d421de3b62c3a',
-  BALANCED_DEX_SCORE: "cxf0276a2413b46d5660e09c4935eecbf401c5811a",
+  iconDebugRpcUrl: "https://berlin.net.solidwallet.io/api/v3d",
+  ADDRESS_PROVIDER_SCORE: 'cx9ac7aeda46931d6acc126d790c06e17f6f6be9aa',
+  BALANCED_DEX_SCORE: "cx4d3b86709c387dec2927158c0377ecabe002f503",
   ledgerBip32Path: "44'/4801368'/0'/0'",
   GOVERNANCE_ADDRESS: "cx0000000000000000000000000000000000000001",
   IISS_API: "cx0000000000000000000000000000000000000000",
-  NID: 83,
+  NID: 7,
   REWARDS_ACCRUE_START: 1628312400, // unix timestamp from when rewards accrue will start
   REWARDS_CLAIMABLE_START: 1628917200, // unix timestamp from when rewards are going to be claimable
   ACTIVATE_REWARDS_TIMESTAMPS: false, // boolean flag deciding if above two timestamp should be respected
   SHOW_BANNER: false,
-  NETWORK: "Sejong"
+  NETWORK: "Berlin"
 };
 
 /*
