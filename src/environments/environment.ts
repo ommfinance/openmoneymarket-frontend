8cd5e8d83f0bee30dcc2d0bd53e0a74c8a8c80a9
robimarkac <robi.markac@gmail.com>
2022-11-07 19:46:10 +0100

    chore: config production env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,21 +3,21 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  iconRpcUrl: "https://lisbon.net.solidwallet.io/api/v3",
+  production: true,
+  iconRpcUrl: "https://ctz.solidwallet.io/api/v3",
   ommRestApi: "https://api.omm.finance/api/v1",
-  iconDebugRpcUrl: "https://lisbon.net.solidwallet.io/api/v3d",
-  ADDRESS_PROVIDER_SCORE: 'cx3beb2fa9b7cfa3684f6db1413897dfcf6cc1b04c',
-  BALANCED_DEX_SCORE: "cx7a90ed2f781876534cf1a04be34e4af026483de4",
+  iconDebugRpcUrl: "https://ctz.solidwallet.io/api/v3d",
+  ADDRESS_PROVIDER_SCORE: 'cx6a66130200b4f08c65ef394469404378ab52e5b6',
+  BALANCED_DEX_SCORE: "cxa0af3165c08318e988cb30993b3048335b94af6c",
   ledgerBip32Path: "44'/4801368'/0'/0'",
   GOVERNANCE_ADDRESS: "cx0000000000000000000000000000000000000001",
   IISS_API: "cx0000000000000000000000000000000000000000",
-  NID: 2,
-  REWARDS_ACCRUE_START: 1628312400, // unix timestamp from when rewards accrue will start
-  REWARDS_CLAIMABLE_START: 1628917200, // unix timestamp from when rewards are going to be claimable
-  ACTIVATE_REWARDS_TIMESTAMPS: false, // boolean flag deciding if above two timestamp should be respected
+  NID: 1,
+  REWARDS_ACCRUE_START: 1629954000, // unix timestamp from when rewards accrue will start
+  REWARDS_CLAIMABLE_START: 1630386000, // unix timestamp from when rewards are going to be claimable
+  ACTIVATE_REWARDS_TIMESTAMPS: true, // boolean flag deciding if above two timestamp should be respected
   SHOW_BANNER: false,
-  NETWORK: "Lisbon"
+  NETWORK: "Mainnet"
 };
 
 /*
